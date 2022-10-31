1d4b10262198ab5f9ae24f2feded9acc05e9d8bd
Volodymyr Cholak <vcholak@users.noreply.github.com>
2022-10-31 12:25:09 -0400

    Using <script setup>.

--- src/views/book/BookList.vue
+++ src/views/book/BookList.vue
@@ -3,6 +3,7 @@
   <div>
     <router-link to="/books/create">Create Book</router-link>
   </div>
+  <div v-if="error">{{ error }}</div>
   <ul>
     <li v-for="book in books" :key="book.id">
       <router-link :to="{name: 'BookDetails', params: {id: book.id}}">{{book.title}}</router-link>
@@ -10,26 +11,23 @@
   </ul>
 </template>
 
-<script>
-export default {
-  name: 'BookList',
-  data () {
-    return {
-      books: [],
-      uri: 'http://localhost:3000/books'
-    }
-  },
-  async mounted () {
-    try {
-      const resp = await fetch(this.uri)
-      const data = await resp.json()
-      this.books = data
-    } catch (err) {
-      console.log(err)
-    }
-  }
+<script setup>
+import { ref, onMounted } from 'vue'
 
-}
+const books = ref([])
+const error = ref(null)
+
+const uri = 'http://localhost:3000/books'
+
+onMounted(async () => {
+  try {
+    const resp = await fetch(uri)
+    const data = await resp.json()
+    books.value = data
+  } catch (err) {
+    error.value = err.message
+  }
+})
 </script>
 
 <style scoped>
