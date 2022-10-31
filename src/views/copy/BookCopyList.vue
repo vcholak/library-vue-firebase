2c4887368521d7af5418ed26858e1959d98b77ae
Volodymyr Cholak <vcholak@users.noreply.github.com>
2022-10-31 14:37:37 -0400

    Using <script setup>.

--- src/views/copy/BookCopyList.vue
+++ src/views/copy/BookCopyList.vue
@@ -3,6 +3,7 @@
   <div>
     <router-link to="/copies/create">Create Book Copy</router-link>
   </div>
+  <div v-if="error">{{ error }}</div>
   <ul>
     <li v-for="copy in copies" :key="copy.id">
       <router-link :to="{name: 'BookCopyDetails', params: {id: copy.id}}">{{copy.bookTitle}} : {{copy.imprint}}</router-link>
@@ -11,24 +12,23 @@
   </ul>
 </template>
 
-<script>
-export default {
-  data () {
-    return {
-      copies: [],
-      uri: 'http://localhost:3000/copies'
-    }
-  },
-  async mounted () {
-    try {
-      const resp = await fetch(this.uri)
-      const data = await resp.json()
-      this.copies = data
-    } catch (err) {
-      console.log(err)
-    }
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const copies = ref([])
+const error = ref(null)
+
+const uri = 'http://localhost:3000/copies'
+
+onMounted(async () => {
+  try {
+    const resp = await fetch(uri)
+    const data = await resp.json()
+    copies.value = data
+  } catch (err) {
+    error.value = err.message
   }
-}
+})
 </script>
 
 <style>
