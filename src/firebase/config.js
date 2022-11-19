68b557c6f93f6325dfbff06ebab252d5b7de0359
Volodymyr Cholak <vcholak@users.noreply.github.com>
2022-11-19 17:04:55 -0500

    Using .env file

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,14 +1,13 @@
 import firebase from 'firebase/compat/app'
 import 'firebase/compat/firestore'
 
-// TODO use .env file
 const firebaseConfig = {
-  apiKey: 'AIzaSyACWI4T2l-7jZkc9A7xkVDHBljZVNMwM-Y',
-  authDomain: 'library-e4a0a.firebaseapp.com',
-  projectId: 'library-e4a0a',
-  storageBucket: 'library-e4a0a.appspot.com',
-  messagingSenderId: '719651866636',
-  appId: '1:719651866636:web:236f0f97e05e81e0bc0629'
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 }
 
 // Initialize Firebase
